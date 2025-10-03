2ba27f6b49b0d35dba9122eac2b1049e1c168c11
David Shubov <116600844+drshooby@users.noreply.github.com>
2025-10-02 21:56:48 -0700

    add: cat func

--- client/functions/fetcher.ts
+++ client/functions/fetcher.ts
@@ -4,9 +4,26 @@ export async function fetchHello() {
     return await res.json();
   } catch (err: unknown) {
     if (err instanceof Error) {
-      return {"error": err.message}
+      return { error: err.message };
     } else {
-      return {"error": "unknown"}
+      return { error: "unknown" };
+    }
+  }
+}
+
+export async function fetchCat() {
+  try {
+    const res = await fetch("/cat");
+    if (!res.ok) {
+      return { error: "Failed to fetch cat" };
+    }
+    const blob = await res.blob();
+    return { url: URL.createObjectURL(blob) };
+  } catch (err: unknown) {
+    if (err instanceof Error) {
+      return { error: err.message };
+    } else {
+      return { error: "unknown" };
     }
   }
 }
