8881665007e0c19964a4959fcab7af439592deef
David Shubov <116600844+drshooby@users.noreply.github.com>
2025-10-04 10:19:58 -0700

    add(https): acm and redirect policies

--- infra/route53.tf
+++ infra/route53.tf
@@ -9,3 +9,20 @@ resource "aws_route53_record" "app_dns" {
     evaluate_target_health = true
   }
 }
+
+resource "aws_route53_record" "cert_validation" {
+  for_each = {
+    for dvo in aws_acm_certificate.cert.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      type   = dvo.resource_record_type
+      record = dvo.resource_record_value
+    }
+  }
+
+  zone_id = var.hosted_zone_id
+  name    = each.value.name
+  type    = each.value.type
+  records = [each.value.record]
+  ttl     = 60
+}
+
